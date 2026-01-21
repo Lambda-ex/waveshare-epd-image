a8ddd377afc17503d26207cee62ea5076608a26d
Lambda-ex <jonas.anderson.main@gmail.com>
2026-01-21 15:52:24 -0700

    demo fix

--- examples/demo.py
+++ examples/demo.py
@@ -7,4 +7,4 @@ sys.path.insert(0, str(ROOT / "src"))
 from epd_image import display_image
 
 print("Displaying image on EPD...")
-display_image("test2.png", mode="fit", rotation=90, model="epd5in65f", refresh=False)
+display_image("test2.jpg", mode="fit", rotation=90, model="epd5in65f", refresh=False)
