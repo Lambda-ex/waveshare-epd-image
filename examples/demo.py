90a4ef2711a9cc918b090c2bb07ff10d1ed06018
Lambda-ex <jonas.anderson.main@gmail.com>
2026-01-21 16:05:11 -0700

    fix git issue

--- examples/demo.py
+++ examples/demo.py
@@ -6,5 +6,5 @@ sys.path.insert(0, str(ROOT / "src"))
 
 from epd_image import display_image
 
-print("Displaying image on EPD...")
+print("Displaying image on EPD...") #
 display_image("test2.jpg", mode="fit", rotation=90, model="epd5in65f", refresh=False)
