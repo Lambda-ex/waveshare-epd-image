7ee5e545ebe56a7c50e2ab2fe37a4305185c84a2
Lambda-ex <jonas.anderson.main@gmail.com>
2026-01-21 17:36:53 -0700

    Release 0.5.0

--- examples/demo.py
+++ examples/demo.py
@@ -1,12 +1,3 @@
-# import sys
-# from pathlib import Path
-
-# TODO : Remove this hack when packaging is done
-# ROOT = Path(__file__).resolve().parents[1]
-# sys.path.insert(0, str(ROOT / "src"))
-
-# try "pip install -e ."
-
 from epd_image import display_image
 
 print("Displaying image on EPD...")
